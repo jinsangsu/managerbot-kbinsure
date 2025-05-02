010587dd2f44a8e91c2ed13f3b535e7d3ae779cf
jinsangsu <licjssj777@gmail.com>
2025-05-02 12:26:24 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def root():
     return {"message": "애순이가 정상 작동 중입니다."}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
